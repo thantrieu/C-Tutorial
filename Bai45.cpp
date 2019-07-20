3b65d1bef3a2d2d4b77dbd5b12b432dcdc27ff7d
Bino <trieuthan92pro@gmail.com>
2019-07-20 21:48:31 +0700

    add lesson 46

---
+++ Bai45.cpp
@@ -0,0 +1,23 @@
+﻿/*
+	Class & Object in C++
+	1. Tổng quan
+	2. Hàm tạo, hàm hủy
+	3. Tính đóng gói: các thành phần private
+	4. Các thành phần pulic
+	5. Từ khóa this
+	6. Thành phần static
+	7 các thành phần const
+	8. Hàm nguyên mẫu
+	9. Lớp nguyên mẫu (template class)
+	10. Hàm bạn
+	11. lớp bạn
+	12. Tính kế thừa: lớp cha, lớp con
+	13. Các thành phần protected
+	14. Q&A: Lớp con kế thừa những gì từ lớp cha?
+	15. Tính đa hình
+	16. Nạp chồng hàm
+	17. Nạp chồng toán tử
+	18. Ghi đè phương thức (override)
+	19. Tính trừu tượng
+	20. Q&A
+*/
---
+++ Bai46.cpp
@@ -0,0 +1,74 @@
+﻿/*
+	Tổng quan về class trong C++
+	- Lý do cần phải có class
+	- Định nghĩa class
+	- Tổng quát cú pháp tạo class
+	- Object tạo ra từ class
+	- Cách sử dụng class
+*/
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+	char ID[20];
+	char name[100];
+	int age;
+	float mark;
+	char address[100];
+
+	void showIdCard() {
+
+	}
+
+	void study(char *subject) {
+
+	}
+
+	void doExample(char* subjName) {
+
+	}
+
+	void payFee(float amount) {
+
+	}
+};
+
+class Cat {
+public:
+	char name[100];
+	int age;
+	float weight;
+	char color[100];
+	char eyesColor[100];
+
+	void move() {
+
+	}
+
+	void sleep() {
+
+	}
+
+	void eat() {
+
+	}
+
+	void speak() {
+
+	}
+};
+
+int main() {
+	
+	Student s1;
+	s1.mark = 8.5;
+	s1.age = 20;
+
+	cout << "Age: " << s1.age << endl;
+
+	Cat cat;
+	cat.name;
+
+	return 0;
+}
