2edd8c56d9cbf1b1932bd69776b2797b8159b93a
Bino <trieuthan92pro@gmail.com>
2019-08-15 22:22:24 +0700

    Add lesson 62

---
+++ Bai62.cpp
@@ -0,0 +1,47 @@
+/*
+	Override
+*/
+
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void move() {
+		cout << "Animal is moving..." << endl;
+	}
+};
+
+class Bird : public Animal {
+public:
+	void move() override {
+		cout << "Bird is flying..." << endl;
+	}
+
+};
+
+class Fish : public Animal {
+public:
+	void move() override {
+		cout << "Fish is swimming..." << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void move() override {
+		cout << "Cat is running on the ground.." << endl;
+	}
+};
+
+int main() {
+	Animal* a = new Bird();
+	a->move();
+	a = new Animal();
+	a->move();
+	a = new Cat();
+	a->move();
+	a = new Fish();
+	a->move();
+	return 0;
+}
